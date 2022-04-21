dafc76c242b309c357d79ed76ecd1c28682f5884
Daryl Lin <da.lin@fi.com>
2022-04-21 12:13:27 -0700

    finished local build with optimizations

--- page_designs/desktop/lendingtree/EQR_Test.js
+++ page_designs/desktop/lendingtree/EQR_Test.js
@@ -6,102 +6,112 @@ import analyticsQA from "../../../../Cloud Brad/scripts/analyticsQA.js";
 
 const service = async (country) => {
 
-    var browser = await Puppeteer.launch({
-        headless: false,
-        defaultViewport: { width: 0, height: 0 }
-    });
+    // Set Browser viewport to maximum for desktop
+    try {
+        var browser = await Puppeteer.launch({
+            headless: false,
+            args: [
+                '--start-maximized' // you can also use '--start-fullscreen'
+            ]
+        });
+        // Use page that initial puppeteer launch creates
+        var pages = await browser.pages();
+        var page = pages[0];
+        //Ensures the page viewport is set to maximum
+        await page.setViewport({
+            width: 0,
+            height: 0,
+        });
+        //Set user agent as BRAD so we don't get blocked by IT
+        page.setUserAgent("FishBot-BRAD");
+    } catch (e) {
+        console.error("Error in Browser setup:", e);
 
-    //Input field here
-    //Class list here
-    // ID list
+    }
 
+    try {
 
-    //End user page progression here
-    //try {
-    //Open up page; set meta data and get browser execution scope
-    var page = await browser.newPage();
-    page.setUserAgent("FishBot-BRAD");
-
-    //Start stepping through
-    await page.goto('https://www.fisherinvestments.com/en-us/campaigns/dgri/lc?PC=PLACEMENTX&CC=XXXX');
-    //Analytics check first page
-    const splashExecutionContext = await page.mainFrame().executionContext();
-    const splashResult = await splashExecutionContext.evaluate(analyticsQA);
-
-
-
-
-    //Click through splash page
-    await page.click("#fb-container > div > div.lightsaber-letter > div > div.brochure-cta > a.btn-text.btn-GetStarted.center-block.hidden-xs");
-
-    //Click through email page
-    await page.waitForSelector(inputSelectors.page_designs.lendingtree.desktop.email);
-    await page.type(inputSelectors.page_designs.lendingtree.desktop.email, "BradTest@fi.com");
-    let form1ExecutionContext = await page.mainFrame().executionContext();
-    let form1Result = await form1ExecutionContext.evaluate(analyticsQA);
-    await page.waitForSelector("#fb-container > div:nth-child(1) > div > div > form > div > div > div > div:nth-child(2) > div > div:nth-child(1) > button");
-    await page.click("#fb-container > div:nth-child(1) > div > div > form > div > div > div > div:nth-child(2) > div > div:nth-child(1) > button");
-
-    //First / Lastname
-    //First name 
-    await page.waitForSelector(inputSelectors.page_designs.lendingtree.desktop.firstName);
-    await page.type(inputSelectors.page_designs.lendingtree.desktop.firstName, "BradTest");
-    //last name
-    await page.type(inputSelectors.page_designs.lendingtree.desktop.lastName, "BradTest");
-    //Analytics Scraper
-    let form2ExecutionContext = await page.mainFrame().executionContext();
-    let form2Result = await form2ExecutionContext.evaluate(analyticsQA);
-    //click on continue button
-    await page.waitForSelector("#singlepageapp-body1 > form > div > div > div > div:nth-child(2) > div > div:nth-child(2) > button");
-    await page.click("#singlepageapp-body1 > form > div > div > div > div:nth-child(2) > div > div:nth-child(2) > button");
-
-    //Autocomplete addrress
-    //key in values 
-    await page.waitForSelector(inputSelectors.page_designs.lendingtree.desktop.address);
-    await page.type(inputSelectors.page_designs.lendingtree.desktop.address, '1450 fas', { delay: 300 });
-    //keyboard down error here
-    await page.keyboard.press("ArrowDown", { delay: 300 });
-    // click enter
-    await page.keyboard.press("Enter", { delay: 300 });
-    //Analytics Scraper
-    let form3ExecutionContext = await page.mainFrame().executionContext();
-    let form3Result = await form3ExecutionContext.evaluate(analyticsQA);
-    // click continue
-    await page.waitForSelector("#singlepageapp-body2 > form > div > div > div > div:nth-child(2) > div > div:nth-child(2) > button");
-    await page.click("#singlepageapp-body2 > form > div > div > div > div:nth-child(2) > div > div:nth-child(2) > button");
-
-
-    //Phone number
-    //fill phone number
-    await page.waitForSelector(inputSelectors.page_designs.lendingtree.desktop.phoneNumber);
-    await page.type(inputSelectors.page_designs.lendingtree.desktop.phoneNumber, '9096075138', { delay: 200 });
-    //analytics qa
-    let form4ExecutionContext = await page.mainFrame().executionContext();
-    let form4Result = await form4ExecutionContext.evaluate(analyticsQA);
-    // click continue
-    await page.waitForSelector("#singlepageapp-body3 > form > div > div > div > div:nth-child(2) > div > div:nth-child(2) > button");
-    await page.click("#singlepageapp-body3 > form > div > div > div > div:nth-child(2) > div > div:nth-child(2) > button");
-
-    //Assets
-    // click assets
-    await page.waitForSelector("#singlepageapp-body4 > form > div > div > div > div:nth-child(1) > div:nth-child(8) > div > div > div:nth-child(5) > label");
-    await page.click("#singlepageapp-body4 > form > div > div > div > div:nth-child(1) > div:nth-child(8) > div > div > div:nth-child(5) > label");
-    //Analytics Scraper
-    let form5ExecutionContext = await page.mainFrame().executionContext();
-    let form5Result = await form5ExecutionContext.evaluate(analyticsQA);
-    // click question
-    await page.click("#singlepageapp-body4 > form > div > div > div > div:nth-child(2) > div > div:nth-child(2) > button");
-
-    //Analytics Scraper
-    let thankYouExecutionContext = await page.mainFrame().executionContext();
-    let thankYouResult = await thankYouExecutionContext.evaluate(analyticsQA);
+        //Start stepping through
+        await page.goto('https://www.fisherinvestments.com/en-us/campaigns/dgri/lc?PC=PLACEMENTX&CC=XXXX');
+        //Analytics check first page
+        const splashExecutionContext = await page.mainFrame().executionContext();
+        const splashResult = await splashExecutionContext.evaluate(analyticsQA);
 
 
 
-    //browser.close();
-    // } catch (e) {
 
-    // }
+        //Click through splash page
+        await page.click("#fb-container > div > div.lightsaber-letter > div > div.brochure-cta > a.btn-text.btn-GetStarted.center-block.hidden-xs");
+
+        //Click through email page
+        await page.waitForSelector(inputSelectors.page_designs.lendingtree.desktop.email);
+        await page.type(inputSelectors.page_designs.lendingtree.desktop.email, "BradTest@fi.com");
+        let form1ExecutionContext = await page.mainFrame().executionContext();
+        let form1Result = await form1ExecutionContext.evaluate(analyticsQA);
+        await page.waitForSelector("#fb-container > div:nth-child(1) > div > div > form > div > div > div > div:nth-child(2) > div > div:nth-child(1) > button");
+        await page.click("#fb-container > div:nth-child(1) > div > div > form > div > div > div > div:nth-child(2) > div > div:nth-child(1) > button");
+
+        //First / Lastname
+        //First name 
+        await page.waitForSelector(inputSelectors.page_designs.lendingtree.desktop.firstName);
+        await page.type(inputSelectors.page_designs.lendingtree.desktop.firstName, "BradTest");
+        //last name
+        await page.type(inputSelectors.page_designs.lendingtree.desktop.lastName, "BradTest");
+        //Analytics Scraper
+        let form2ExecutionContext = await page.mainFrame().executionContext();
+        let form2Result = await form2ExecutionContext.evaluate(analyticsQA);
+        //click on continue button
+        await page.waitForSelector("#singlepageapp-body1 > form > div > div > div > div:nth-child(2) > div > div:nth-child(2) > button");
+        await page.click("#singlepageapp-body1 > form > div > div > div > div:nth-child(2) > div > div:nth-child(2) > button");
+
+        //Autocomplete addrress
+        //key in values 
+        await page.waitForSelector(inputSelectors.page_designs.lendingtree.desktop.address);
+        await page.type(inputSelectors.page_designs.lendingtree.desktop.address, '1450 fas', { delay: 300 });
+        //keyboard down error here
+        await page.keyboard.press("ArrowDown", { delay: 300 });
+        // click enter
+        await page.keyboard.press("Enter", { delay: 300 });
+        //Analytics Scraper
+        let form3ExecutionContext = await page.mainFrame().executionContext();
+        let form3Result = await form3ExecutionContext.evaluate(analyticsQA);
+        // click continue
+        await page.waitForSelector("#singlepageapp-body2 > form > div > div > div > div:nth-child(2) > div > div:nth-child(2) > button");
+        await page.click("#singlepageapp-body2 > form > div > div > div > div:nth-child(2) > div > div:nth-child(2) > button");
+
+
+        //Phone number
+        //fill phone number
+        await page.waitForSelector(inputSelectors.page_designs.lendingtree.desktop.phoneNumber);
+        await page.type(inputSelectors.page_designs.lendingtree.desktop.phoneNumber, '9096075138', { delay: 200 });
+        //analytics qa
+        let form4ExecutionContext = await page.mainFrame().executionContext();
+        let form4Result = await form4ExecutionContext.evaluate(analyticsQA);
+        // click continue
+        await page.waitForSelector("#singlepageapp-body3 > form > div > div > div > div:nth-child(2) > div > div:nth-child(2) > button");
+        await page.click("#singlepageapp-body3 > form > div > div > div > div:nth-child(2) > div > div:nth-child(2) > button");
+
+        //Assets
+        // click assets
+        await page.waitForSelector("#singlepageapp-body4 > form > div > div > div > div:nth-child(1) > div:nth-child(8) > div > div > div:nth-child(5) > label");
+        await page.click("#singlepageapp-body4 > form > div > div > div > div:nth-child(1) > div:nth-child(8) > div > div > div:nth-child(5) > label");
+        //Analytics Scraper
+        let form5ExecutionContext = await page.mainFrame().executionContext();
+        let form5Result = await form5ExecutionContext.evaluate(analyticsQA);
+        // click question
+        await page.click("#singlepageapp-body4 > form > div > div > div > div:nth-child(2) > div > div:nth-child(2) > button");
+
+        //Analytics Scraper
+        // response quality should be trigered here
+        let thankYouExecutionContext = await page.mainFrame().executionContext();
+        let thankYouResult = await thankYouExecutionContext.evaluate(analyticsQA);
+
+
+
+        //browser.close();
+    } catch (e) {
+        console.error("Error in Browser step through:", e);
+    }
 
     const analyticsResults = thankYouResult;
 
