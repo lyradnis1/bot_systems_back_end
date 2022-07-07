6c06bd10713da2730963a1ed68b3f401ae86f7b7
Daryl Lin <da.lin@fi.com>
2022-07-07 13:15:52 -0700

    capitalized letter

--- utility/APIutility/qa_get_request.js
+++ utility/APIutility/qa_get_request.js
@@ -1,15 +1,15 @@
 import designUrls from "../page_design_urls.js";
 
-import quickenloans_lendingtree_pf_slideshow_desktop_test from "../../testing_suite/desktop/quickenloans_lendingtree_pf_slideshow.js";
-import quickenloans_lendingtree_pf_subline_desktop_test from "../../testing_suite/desktop/quickenloans_lendingtree_pf_subline.js";
-import quickenloans_lendingtree_pf_desktop_test from "../../testing_suite/desktop/quickenloans_lendingtree_pf.js";
+import quickenloans_lendingtree_pf_slideshow_desktop_test from "../../testing_suite/desktop/quickenloans_lendingtree_PF_slideshow.js";
+import quickenloans_lendingtree_pf_subline_desktop_test from "../../testing_suite/desktop/quickenloans_lendingtree_PF_subline.js";
+import quickenloans_lendingtree_pf_desktop_test from "../../testing_suite/desktop/quickenloans_lendingtree_PF.js";
 import quickenloans_lightweight_desktop_test from "../../testing_suite/desktop/quickenloans_lightweight.js";
 
-import quickenloans_lendingtree_pf_slideshow_mobile_test from "../../testing_suite/mobile/quickenloans_lendingtree_pf_slideshow.js";
-import quickenloans_lendingtree_pf_subline_mobile_test from "../../testing_suite/mobile/quickenloans_lendingtree_pf_subline.js";
-import quickenloans_lendingtree_pf_mobile_test from "../../testing_suite/mobile/quickenloans_lendingtree_pf.js";
+import quickenloans_lendingtree_pf_slideshow_mobile_test from "../../testing_suite/mobile/quickenloans_lendingtree_PF_slideshow.js";
+import quickenloans_lendingtree_pf_subline_mobile_test from "../../testing_suite/mobile/quickenloans_lendingtree_PF_subline.js";
+import quickenloans_lendingtree_pf_mobile_test from "../../testing_suite/mobile/quickenloans_lendingtree_PF.js";
 import quickenloans_lightweight_mobile_test from "../../testing_suite/mobile/quickenloans_lightweight.js";
-import lendingtree_pf_mobile_test from "../../testing_suite/mobile/lendingtree_pf.js";
+import lendingtree_pf_mobile_test from "../../testing_suite/mobile/lendingtree_PF.js";
 
 
 
