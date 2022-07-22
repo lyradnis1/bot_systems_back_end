e7f6e9111cf466dcfc2fb540458b2abddb9e3e31
Daryl Lin <da.lin@fi.com>
2022-07-22 12:39:36 -0700

    removed browser arguments from fx

--- utility/async_disposer_methods.js
+++ utility/async_disposer_methods.js
@@ -14,6 +14,14 @@ const withBrowser = async function (fn) {
         headless: global_parameters.headless,
         defaultViewport: null,
         slowMo: global_parameters.slowmo,
+        // '--no-sandbox',
+        //     '--disable-setuid-sandbox',
+        //     '--disable-dev-shm-usage',
+        //     '--disable-accelerated-2d-canvas',
+        //     '--no-first-run',
+        //     '--no-zygote',
+        //     //'--single-process', // <- this one doesn't works in Windows
+        //     '--disable-gpu'
         args: [
             '--disable-gpu',
             '--disable-dev-shm-usage',
