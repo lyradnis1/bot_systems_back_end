97aa839a236214f649087b2daf70c47c598a42cb
Daryl Lin <da.lin@fi.com>
2022-08-22 13:43:40 -0700

    Reducing back to 4 concurrency

--- utility/global_parameters.js
+++ utility/global_parameters.js
@@ -3,7 +3,7 @@ export default {
     headless: true,
     //100 is good version
     slowmo: 100,
-    concurrency: 7,
+    concurrency: 4,
     //Options for domain can be preview1, preview2, uat1, uat2, prod
     //Technically you can put anything that is not preview1/preview2/uat1/uat2 since the default is always going to be prod
     subdomain: "prod"
